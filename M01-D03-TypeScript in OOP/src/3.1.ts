d1b73a887c5a8d2b79e927b24064b5391edd921d
RU063 <rashedinislam.06@gmail.com>
2024-11-07 18:28:49 +0600

    mission 1 day 3 - typeScript in OOP begins

---
+++ M01-D03-TypeScript in OOP/src/3.1.ts
@@ -0,0 +1,28 @@
+{
+  //
+
+  // OOP --> Class
+
+  class Animal {
+    constructor(public name: string, public species : string, public sound: string) {
+    }
+
+    makeSound() {
+      console.log(`The ${this.name} sounds ${this.sound}`)
+      
+    }
+}
+
+  const dog = new Animal('Alshesian', 'dog', 'ghew ghew')
+
+  console.log(dog.makeSound())
+
+  const cat = new Animal('Pet', 'cat', 'meaw meaw')
+  console.log(cat.makeSound());
+
+  
+  
+  
+
+  //
+}
